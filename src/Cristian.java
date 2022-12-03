93f5375840f069802e928c9ecb026123dedee09c
Cristianttt21 <paezmartinez866@gmail.com>
2022-12-03 15:01:40 -0500

    Cambios

--- src/Cristian.java
+++ src/Cristian.java
@@ -2,14 +2,7 @@
 public class Cristian {
 
 	public static void main(String[] args) {
-		System.out.println("Este es un mensaje nuevo");
-		System.out.println("Prueba de actualizacion 1");
-		System.out.println("prueba de subida1");
-		System.out.println("prueba de subida3");
-		System.out.println("prueba de subida7");
-		int x, j;
 		
-		System.out.println("subida odncqipcnqpc");
 	}
 
 }
