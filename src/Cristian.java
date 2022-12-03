fa9f8a4caef2ebe318393869a2e376a48f1b4016
Mateperry <castromateo2004@gmail.com>
2022-12-03 15:35:35 -0500

    Yaaaaaaa aprendi

--- src/Cristian.java
+++ src/Cristian.java
@@ -4,6 +4,6 @@ public class Cristian {
 	public static void main(String[] args) {
 		System.out.println("hola mateo");
 		System.out.println("prrrrrrrrrrrrrrrrrrraivnqpvnqepvnq");
-
+		System.out.println("Holaaa putooo"); 
 	}
 }
